1762e504eeb42f263cf56cf43bfa6ac556c08068
zhang5171291 <zhangbin5171291@163.com>
2012-08-15 17:51:24 +0800

    设计模式测试项目

---
+++ DesignPattern/ClassLibrary/创建型模式/单态模式/SingletonPattern.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public sealed class SingletonPattern
+    {
+        private SingletonPattern()
+        {
+        }
+
+        public string name { get; set; }
+
+        private static SingletonPattern _obj;
+        private static readonly Object LockObj = new object();
+        public static SingletonPattern GetInstance
+        {
+            get
+            {
+                if (_obj == null)
+                    lock (LockObj)
+                        if (_obj == null)
+                            _obj = new SingletonPattern();
+                return _obj;
+            }
+        }
+
+    }
+}
---
+++ DesignPattern/ConsoleApplication/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ClassLibrary;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecuteDesignPattern(DesignPatternType.单态模式);
+
+            Console.Read();
+        }
+
+        /// <summary>
+        /// 执行设计模式
+        /// </summary>
+        /// <param name="designPatternType"></param>
+        private static void ExecuteDesignPattern(DesignPatternType designPatternType)
+        {
+            Console.WriteLine("-> {0} 开始",designPatternType.ToString());
+            switch (designPatternType)
+            {
+                case DesignPatternType.单态模式:
+                    Console.WriteLine("写入属性值 “我是张斌!” ");
+                    SingletonPattern.GetInstance.name = "我是张斌!";
+                    Console.WriteLine("读出属性值 {0}", SingletonPattern.GetInstance.name);
+                    break;
+
+            }
+            Console.WriteLine("-> {0} 结束", designPatternType.ToString());
+        }
+
+
+
+    }
+
+    /// <summary>
+    /// 设计模式类型
+    /// </summary>
+    public enum DesignPatternType
+    {
+        #region 创建型模式
+        抽象工厂,
+        单态模式,
+        工厂方法,
+        建造者模式,
+        原型模式,
+        #endregion
+
+        #region 行为性模式
+        备忘录模式,
+        策略模式,
+        迭代器模式,
+        访问者模式,
+        观察者模式,
+        解释器模式,
+        命令模式,
+        模板模式,
+        责任链模式,
+        中介者模式,
+        状态模式,
+        #endregion
+
+        #region 结构型模式
+        代理模式,
+        亨元模式,
+        桥接模式,
+        适配器模式,
+        外观模式,
+        装饰模式,
+        组合模式
+        #endregion
+    }
+}
